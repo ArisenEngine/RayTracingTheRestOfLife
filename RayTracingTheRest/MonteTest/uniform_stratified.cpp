d207d92a38f0571bff167b6c01638991dfd4441a
jingwen.wang <jingwen.wang@dena.com>
2023-12-14 18:54:48 +0800

    test

--- RayTracingTheRest/MonteTest/uniform_stratified.cpp
+++ RayTracingTheRest/MonteTest/uniform_stratified.cpp
@@ -12,34 +12,17 @@
 #endif
 
 int main() {
-    int inside_circle = 0;
-    int inside_circle_stratified = 0;
-    int sqrt_N = 100;
-    int size = 500;
+   
+    const int size = 500;
+    const int sample_count = 1000;
     graph uniform_graph(size, size, "uniform");
     graph stratified_graph(size, size, "stratified");
 
-    for (int i = 0; i < sqrt_N; i++) {
-        for (int j = 0; j < sqrt_N; j++) {
-            auto x = random_double(-1, 1);
-            auto y = random_double(-1, 1);
-            uniform_graph.draw_point(x * 0.5 + 0.5, y * 0.5 + 0.5);
-            if (x * x + y * y < 1)
-                inside_circle++;
-            x = 2 * ((i + random_double()) / sqrt_N) - 1;
-            y = 2 * ((j + random_double()) / sqrt_N) - 1;
-            stratified_graph.draw_point(x * 0.5 + 0.5, y * 0.5 + 0.5);
-            if (x * x + y * y < 1)
-                inside_circle_stratified++;
-        }
+    for (int i = 0; i < sample_count; ++i)
+    {
+        uniform_graph.draw_point(random_double(0, 1), random_double(0, 1));
     }
-
-    std::cout << std::fixed << std::setprecision(12);
-    std::cout
-        << "Regular    Estimate of Pi = "
-        << (4.0 * inside_circle) / (sqrt_N * sqrt_N) << '\n'
-        << "Stratified Estimate of Pi = "
-        << (4.0 * inside_circle_stratified) / (sqrt_N * sqrt_N) << '\n';
+   
 
     while (true)
     {
