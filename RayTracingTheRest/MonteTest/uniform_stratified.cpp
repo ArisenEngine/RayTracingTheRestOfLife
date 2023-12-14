8cd93bdaba3fe2fe5f97361a1529484f47b01d7b
eatdreamcat <meow.std@outlook.com>
2023-12-14 19:15:11 +0800

    cornel box

--- RayTracingTheRest/MonteTest/uniform_stratified.cpp
+++ RayTracingTheRest/MonteTest/uniform_stratified.cpp
@@ -1,33 +1,33 @@
 
 
-#include "../rt_weekend.h"
-#include "../graph.h"
-#include <iostream>
-#include <iomanip>
-
-#if _DEBUG
-#pragma comment(lib,"opencv_world481d.lib")
-#else
-#pragma comment(lib,"opencv_world481.lib")
-#endif
-
-int main() {
-   
-    const int size = 500;
-    const int sample_count = 1000;
-    graph uniform_graph(size, size, "uniform");
-    graph stratified_graph(size, size, "stratified");
-
-    for (int i = 0; i < sample_count; ++i)
-    {
-        uniform_graph.draw_point(random_double(0, 1), random_double(0, 1));
-    }
-   
-
-    while (true)
-    {
-        uniform_graph.present();
-        stratified_graph.present();
-        cv::pollKey();
-    }
-}
+//#include "../rt_weekend.h"
+//#include "../graph.h"
+//#include <iostream>
+//#include <iomanip>
+//
+//#if _DEBUG
+//#pragma comment(lib,"opencv_world481d.lib")
+//#else
+//#pragma comment(lib,"opencv_world481.lib")
+//#endif
+//
+//int main() {
+//   
+//    const int size = 500;
+//    const int sample_count = 1000;
+//    graph uniform_graph(size, size, "uniform");
+//    graph stratified_graph(size, size, "stratified");
+//
+//    for (int i = 0; i < sample_count; ++i)
+//    {
+//        uniform_graph.draw_point(random_double(0, 1), random_double(0, 1));
+//    }
+//   
+//
+//    while (true)
+//    {
+//        uniform_graph.present();
+//        stratified_graph.present();
+//        cv::pollKey();
+//    }
+//}
